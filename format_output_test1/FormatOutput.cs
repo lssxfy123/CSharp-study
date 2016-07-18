19d4ed3d4b8c754bd9fcb476dbecfeb5029d6a9e
lssxfy123 <liushenshenxfy@126.com>
2016-07-18 18:08:51 +0800

    C#学习2：格式输出，隐式类型，字面量

---
+++ format_output_test1/FormatOutput.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace format_output_test1
+{
+    class FormatOutput
+    {
+        // WriteLine("format string", arg0, arg1,...,argN);
+        // 格式说明符{argnum, width:fmt}
+        static void Main(string[] args)
+        {
+            Console.WriteLine("February has {0} or {1}", 28, 29);
+
+            // February has         28 or    29
+            Console.WriteLine("February has {0,10} or {1,5}", 28, 29);
+
+            // Value   Squared Cubed
+            // 1       1       1
+            // 2       4       8
+            // 3       9       27
+            // 4       16      64
+            // 5       25      125
+            // 6       36      216
+            // 7       49      343
+            // 8       64      512
+            // 9       81      729
+            Console.WriteLine("Value\tSquared\tCubed");
+            for (int i = 1; i < 10; ++i)
+            {
+                Console.WriteLine("{0}\t{1}\t{2}", i, i * i, i * i * i);
+            }
+
+            // 同C++类似，如果写成10/3则结果为3
+            Console.WriteLine("Here is 10 / 3: {0:#.##}", 10.0 / 3);
+
+            // Here is 10 / 3: 33.33
+            Console.WriteLine("Here is 10 / 3: {0:#.##}", 100.0 / 3);
+
+            // 123,456.56
+            Console.WriteLine("{0:###,###.##}", 123456.56);
+        }
+    }
+}
---
+++ implicitly_typed_variable_test1/VarTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace implicitly_typed_variable_test1
+{
+    class VarTest
+    {
+        static void Main(string[] args)
+        {
+            var s1 = 4.0;
+            Console.WriteLine("type is {0}", s1.GetType());  // System.Double
+            var s2 = 5.0;
+
+            var hypot = Math.Sqrt((s1 * s1) + (s2 * s2));
+            Console.WriteLine("{0:#.###}", hypot);
+
+            // s1 = 12.2M; // error，s1的类型已确定，是double型
+        }
+    }
+}
