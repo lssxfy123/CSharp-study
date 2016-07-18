7d703085405f1c0f2e3fc8a19f100477e70b80e2
lssxfy123 <liushenshenxfy@126.com>
2016-07-18 16:48:22 +0800

    c#学习1

---
+++ char_test1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace char_test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("char in C# size is {0} bytes", sizeof(char));  // 2
+        }
+    }
+}
