52d28d6afc65c18959a40070ad78b9e86384171f
lssxfy123 <liushenshenxfy@126.com>
2016-07-25 21:22:29 +0800

    C#泛型约束

---
+++ generics_test4/GenericsTest.cs
@@ -0,0 +1,48 @@
+﻿// Copyright 2016.刘珅珅
+// author：刘珅珅
+// 泛型约束：new()约束
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics_test4
+{
+    class MyClass
+    { }
+
+    class MyClass2
+    {
+        public MyClass2()
+        {
+
+        }
+
+        public MyClass2(int i)
+        {
+
+        }
+    }
+
+    class Gen<T> where T : new()
+    {
+        T obj;
+        public Gen()
+        {
+            obj = new T();
+        }
+    }
+
+    class GenericsTest
+    {
+        static void Main(string[] args)
+        {
+            // MyClass有默认的构造函数
+            Gen<MyClass> x = new Gen<MyClass>();
+
+            // MyClass2必须显示提供一个无参构造函数
+            Gen<MyClass2> y = new Gen<MyClass2>();
+        }
+    }
+}
---
+++ generics_test5/GenericsTest.cs
@@ -0,0 +1,51 @@
+﻿// Copyright 2016.刘珅珅
+// author：刘珅珅
+// 泛型约束：引用类型约束和值类型约束
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics_test5
+{
+    class MyClass { }
+
+    struct MyStruct { }
+
+    class Gen<T> where T : class
+    {
+        T obj;
+        public Gen()
+        {
+            obj = null;
+        }
+    }
+
+    class Gen1<T> where T : struct
+    {
+        T obj;
+
+        public Gen1(T x)
+        {
+            obj = x;
+        }
+    }
+
+    class GenericsTest
+    {
+        static void Main(string[] args)
+        {
+            Gen<MyClass> x = new Gen<MyClass>();
+
+            // error，类型实参必须为引用类型
+            // Gen<int> x1 = new Gen<int>();
+
+            Gen1<int> y = new Gen1<int>(5);
+            Gen1<MyStruct> y1 = new Gen1<MyStruct>(new MyStruct());
+
+            // error，类型实参必须为值类型
+            // Gen1<MyClass> y2 = new Gen1<MyClass>(new MyClass());
+        }
+    }
+}
