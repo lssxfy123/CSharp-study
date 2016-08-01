14b6735ab22fee4a7e16082eb4931438efd449d5
lssxfy123 <liushenshenxfy@126.com>
2016-08-01 21:14:44 +0800

    C#任务的等待和TaskFactory，Lambda表达式定义任务

---
+++ task_test3/TaskTestcs.cs
@@ -0,0 +1,50 @@
+﻿// Copyright 2016.刘珅珅
+// author：刘珅珅
+// 任务的等待
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace task_test3
+{
+    class TaskTestcs
+    {
+        // 任务函数
+        static void MyTask()
+        {
+            Console.WriteLine("MyTask() #" + Task.CurrentId + " starting.");
+
+            for (int i = 0; i < 5; ++i)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("In MyTask() #" + Task.CurrentId + ", count is " + i);
+            }
+
+            Console.WriteLine("MyTask #" + Task.CurrentId + " terminating");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main thread starting.");
+
+            Task task1 = new Task(MyTask);
+            Task task2 = new Task(MyTask);
+
+            // 运行任务
+            task1.Start();
+            task2.Start();
+
+            Console.WriteLine("Task ID for task1 is " + task1.Id);
+            Console.WriteLine("Task ID for task2 is " + task2.Id);
+
+            // 暂停Main()直到task1和task2完成
+            task1.Wait();
+            task2.Wait();
+
+            Console.WriteLine("Main thread ending.");
+        }
+    }
+}
---
+++ task_test4/TaskTest.cs
@@ -0,0 +1,46 @@
+﻿// Copyright 2016.刘珅珅
+// author：刘珅珅
+// TaskFactory与Lambda表达式
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace task_test4
+{
+    class TaskTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main thread starting.");
+
+            // 使用TaskFactory任务工厂启动任务
+            // 使用Lambda表达式定义一个任务入口方法
+            Task task = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Task starting.");
+
+                for (int i = 0; i < 10; ++i)
+                {
+                    Thread.Sleep(500);
+                    Console.WriteLine("Task count is " + i);
+                }
+
+                Console.WriteLine("Task terminating");
+            });
+
+            // 等待直到任务完成
+            task.Wait();
+
+            // 任务的清除
+            // Dispose()方法必须在Wait()或类似的表示
+            // 任务完成的方法调用完成之后才能调用
+            // Dispose()方法主要用于创建并放弃许多任务的程序
+            task.Dispose();
+
+            Console.WriteLine("Main thread ending.");
+        }
+    }
+}
