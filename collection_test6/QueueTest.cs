36593478e7cdfedb665030c2be622be309b49579
lssxfy123 <liushenshenxfy@126.com>
2016-08-08 21:44:50 +0800

    C# Queue类和BitArray类

---
+++ collection_test6/QueueTest.cs
@@ -0,0 +1,60 @@
+﻿// Copyright 2016.刘珅珅
+// author：刘珅珅
+// Queue：队列
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collection_test6
+{
+    class QueueTest
+    {
+        static void ShowEnq(Queue q, int a)
+        {
+            q.Enqueue(a);
+            Console.WriteLine("Enqueue(" + a + ")");
+
+            Console.Write("queue: ");
+            foreach (int i in q)
+                Console.Write(i + " ");
+
+            Console.WriteLine();
+        }
+
+        static void ShowDeq(Queue q)
+        {
+            Console.Write("Dequeue -> ");
+            int a = (int)q.Dequeue();
+            Console.WriteLine(a);
+
+            Console.Write("queue: ");
+            foreach (int i in q)
+                Console.Write(i + " ");
+
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+
+            ShowEnq(q, 22);
+            ShowEnq(q, 33);
+            ShowEnq(q, 44);
+            ShowDeq(q);
+            ShowDeq(q);
+            ShowDeq(q);
+
+            try
+            {
+                ShowDeq(q);
+            } catch (InvalidOperationException)
+            {
+                Console.WriteLine("Queue empty.");
+            }
+        }
+    }
+}
---
+++ collection_test7/BitArrayTest.cs
@@ -0,0 +1,39 @@
+﻿// Copyright 2016.刘珅珅
+// author：刘珅珅
+// BitArray：位存储
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collection_test7
+{
+    class BitArrayTest
+    {
+        static void ShowBits(string rem, BitArray bits)
+        {
+            Console.WriteLine(rem);
+            for (int i = 0; i < bits.Count; ++i)
+                Console.Write("{0, -6}", bits[i]);
+            Console.WriteLine("\n");
+        }
+
+        static void Main(string[] args)
+        {
+            BitArray ba1 = new BitArray(8);
+            byte[] b = { 67};
+            BitArray ba2 = new BitArray(b);
+
+            ShowBits("Original contents of ba1: ", ba1);
+            ba1 = ba1.Not();
+            ShowBits("Contents of ba1 after Not:", ba1);
+
+            // 67的二进制补码为：01000011
+            // 输出为True True False False False False True False
+            // 从低位开始输出
+            ShowBits("Contents of ba2:", ba2);
+        }
+    }
+}
