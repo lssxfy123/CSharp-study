3e5c54912741a7a8bbb175ebeff414c71e2e2351
lssxfy123 <liushenshenxfy@126.com>
2016-08-09 21:10:16 +0800

    泛型结合类List和LinkedList

---
+++ collection_test8/List.cs
@@ -0,0 +1,40 @@
+﻿// Copyright 2016.刘珅珅
+// author：刘珅珅
+// 泛型动态数组
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collection_test8
+{
+    class List
+    {
+        static void Main(string[] args)
+        {
+            Queue<char> queue = new Queue<char>();
+            queue.Enqueue('a');
+            queue.Enqueue('b');
+            queue.Enqueue('c');
+
+            // 可以用队列Queue<T>给List传参
+            // Queue<T>实现了IEnumerable<T>
+            List<char> list = new List<char>(queue);
+
+            Console.Write("Current contents: ");
+            for (int i = 0; i < list.Count; ++i)
+                Console.Write(list[i] + " ");
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Removing 1 element.");
+            list.Remove('c');
+
+            Console.Write("Contents: ");
+            foreach (char c in list)
+                Console.Write(c + " ");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ collection_test9/LinkedListTest.cs
@@ -0,0 +1,47 @@
+﻿// Copyright 2016.刘珅珅
+// author：刘珅珅
+// 泛型双向链表
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collection_test9
+{
+    class LinkedListTest
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<char> linked_list = new LinkedList<char>();
+            linked_list.AddFirst('A');
+            linked_list.AddFirst('B');
+            linked_list.AddFirst('C');
+            linked_list.AddFirst('D');
+            linked_list.AddFirst('E');
+
+            // 结点方式
+            Console.Write("Display contents by following links: ");
+            for (LinkedListNode<char> node = linked_list.First; node != null; node = node.Next)
+                Console.Write(node.Value + " ");
+            Console.WriteLine("\n");
+
+            Console.Write("Display contents with foreach loop: ");
+            foreach (char c in linked_list)
+                Console.Write(c + " ");
+            Console.WriteLine("\n");
+
+            // 倒序显示
+            Console.Write("Display contents backwards: ");
+            for (LinkedListNode<char> node = linked_list.Last; node != null; node = node.Previous)
+                Console.Write(node.Value + " ");
+            Console.WriteLine("\n");
+
+            linked_list.AddLast('K');
+            Console.Write("Contents after addition to end: ");
+            foreach (char c in linked_list)
+                Console.Write(c + " ");
+            Console.WriteLine("\n");
+        }
+    }
+}
