52dde2e15a87876cb804d710a56b401de1c1118e
lssxfy123 <liushenshenxfy@126.com>
2016-08-11 21:03:49 +0800

    C#泛型集合

---
+++ collection_test10/DictionaryTest.cs
@@ -0,0 +1,31 @@
+﻿// Copyright 2016.刘珅珅
+// author：刘珅珅
+// 泛型字典类：Dictionary
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collection_test10
+{
+    class DictionaryTest
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> dict = new Dictionary<string, double>();
+
+            dict.Add("Bulter, John", 730);
+            dict.Add("Swartz, Sarah", 590);
+            dict.Add("Pyke, Thoms", 450);
+
+            // 不能添加相同的key值
+            // dict.Add("Bulter, John", 730);
+
+            ICollection<string> keys = dict.Keys;
+
+            foreach (string str in keys)
+                Console.WriteLine("{0}, Salary: {1:C}", str, dict[str]);
+        }
+    }
+}
---
+++ collection_test11/HashSetTest.cs
@@ -0,0 +1,51 @@
+﻿// Copyright 2016.刘珅珅
+// author：刘珅珅
+// HashSet：散列集
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collection_test11
+{
+    class HashSetTest
+    {
+        static void Show(string msg, HashSet<char> set)
+        {
+            Console.Write(msg);
+            foreach (char c in set)
+                Console.Write(c + " ");
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            HashSet<char> hashSet1 = new HashSet<char>();
+            HashSet<char> hashSet2 = new HashSet<char>();
+
+            hashSet1.Add('A');
+            hashSet1.Add('B');
+            hashSet1.Add('C');
+
+            hashSet2.Add('C');
+            hashSet2.Add('D');
+            hashSet2.Add('E');
+
+            Show("Initial content of hashSet1: ", hashSet1);
+            Show("Initial content of hashSet2: ", hashSet2);
+
+            // 两个集合的不同元素
+            hashSet1.SymmetricExceptWith(hashSet2);
+            Show("hashSet1 after Symmetric difference with hashSet2: ", hashSet1);
+
+            // 两个集合的并集
+            hashSet1.UnionWith(hashSet2);
+            Show("hashSet1 after union with hashSet2: ", hashSet1);
+
+            // 一个集合剔除掉另一个集合
+            hashSet1.ExceptWith(hashSet2);
+            Show("hashSet1 after subtracting with hashSet2: ", hashSet1);
+        }
+    }
+}
