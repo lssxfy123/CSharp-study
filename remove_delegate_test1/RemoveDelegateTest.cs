bb1e2de4097490dad55eca881e5a3e8215bcae28
lssxfy123 <liushenshenxfy@126.com>
2016-08-24 18:03:29 +0800

    XAML中 的布局

---
+++ remove_delegate_test1/RemoveDelegateTest.cs
@@ -0,0 +1,24 @@
+﻿// Copyright 2016.刘珅珅
+// author：刘珅珅
+// 移除事件的所有委托
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace remove_delegate_test1
+{
+    class MyClass
+    {
+        public event EventHandler<int> SomeEvent;
+    }
+
+    class RemoveDelegateTest
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
