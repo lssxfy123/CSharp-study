7da25dee04a2280ade7835eca157d092e277197b
lssxfy123 <liushenshenxfy@126.com>
2016-09-03 15:34:02 +0800

    依赖属性

---
+++ dependency_property_test2/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dependency_property_test2
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Student student = new Student();
+
+            // 设置绑定的数据源和Path
+            // 数据源为textBox1，Path为Text属性
+            student.SetBinding(Student.NameProperty, new Binding("Text") { Source = textBox1});
+
+            // 数据源为student，Path为Name属性
+            textBox2.SetBinding(TextBox.TextProperty, new Binding("Name") { Source = student});
+        }
+    }
+}
---
+++ dependency_property_test2/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace dependency_property_test2
+{
+    class Student:DependencyObject
+    {
+        // CLR属性包装器
+        public string Name
+        {
+            get { return (string)GetValue(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+        
+        // 依赖属性
+        // Register函数的3个参数
+        // 第1个参数为string类型，指明以哪个CLR属性作为依赖属性的包装器
+        // 第2个参数用来指明依赖属性存储什么类型的值
+        // 第3个参数指明依赖属性的宿主类型或者说依赖属性注册关联到哪个类型
+        public static readonly DependencyProperty NameProperty =
+            DependencyProperty.Register("Name", typeof(string), typeof(Student));
+
+        // SetBinding包装
+        public BindingExpressionBase SetBinding(DependencyProperty dp, BindingBase binding)
+        {
+            return BindingOperations.SetBinding(this, dp, binding);
+        }
+    }
+}
