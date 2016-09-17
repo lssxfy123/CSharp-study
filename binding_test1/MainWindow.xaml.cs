f2054b19962e2f873f3a00d69f78bced6c394953
lssxfy123 <liushenshenxfy@126.com>
2016-09-17 14:59:24 +0800

    数据绑定

---
+++ binding_test1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace binding_test1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Student dataSource;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // 准备数据源
+            dataSource = new Student();
+
+            // 准备绑定
+            Binding binding = new Binding();
+            binding.Source = dataSource;
+
+            // 绑定到数据源的哪个属性
+            binding.Path = new PropertyPath("Name");
+
+            // 连接数据源和目标
+            BindingOperations.SetBinding(textBoxName, TextBox.TextProperty, binding);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            dataSource.Name += "Name";
+        }
+    }
+}
---
+++ binding_test1/Student.cs
@@ -0,0 +1,33 @@
+﻿// Copyright 2016.刘珅珅
+// author：刘珅珅
+// 数据绑定：数据源
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binding_test1
+{
+    class Student:INotifyPropertyChanged
+    {
+        // 属性改变事件
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Name"));
+                }
+            }
+        }
+    }
+}
