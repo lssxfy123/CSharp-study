52def917b16ca07ee3d7edd29edfa49b6574550c
lssxfy123 <liushenshenxfy@126.com>
2016-11-04 18:07:54 +0800

    C#在一个进程中启动另一个进程

---
+++ process_test1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace process_test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.ProcessStartInfo info = new System.Diagnostics.ProcessStartInfo();
+            info.FileName = "WindowsTest.exe";
+            info.WindowStyle = System.Diagnostics.ProcessWindowStyle.Minimized;
+
+            try
+            {
+                System.Diagnostics.Process.Start(info);
+            }
+            catch (Exception e)
+            {
+                
+            }
+
+            while (true)
+            {
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
