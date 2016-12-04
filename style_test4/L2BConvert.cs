8d57f6384dc445cfec50b4df69582a1f5854e9c1
lssxfy123 <liushenshenxfy@126.com>
2016-12-04 14:36:56 +0800

    触发器

---
+++ style_test4/L2BConvert.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace style_test4
+{
+    public class L2BConvert:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int textLength = (int)value;
+            return textLength > 6 ? true : false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ style_test5/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace style_test5
+{
+    public class Student
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+    }
+}
