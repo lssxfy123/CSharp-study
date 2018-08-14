3257e83c4163f4d8160eabe5e878e986174cf4de
lssxfy123 <liushenshenxfy@126.com>
2018-08-14 15:39:26 +0800

    string format测试双重引号

---
+++ string_format_test/Program.cs
@@ -0,0 +1,17 @@
+﻿// string.Format双重引号
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace string_format_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tmp = string.Format("abcde{0}, aeeljl'{1}'aaljal\"{2}\"ljljl", "ABC", "DEF", "OMG");
+            Console.WriteLine(tmp);
+        }
+    }
+}
